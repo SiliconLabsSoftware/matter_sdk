mykrupp/connectedhomeip

Pull request #1966: Applied changes to prevent loss of data when unlatching on 917
Merge in WMN_TOOLS/matter from bugfix/loosing_pin_unlatch_fix to silabs_slc_1.3

Squashed commit of the following:

commit 22e972cc134eb145aa3cf0e99c920975efbcf1e8
Author: lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
Date:   Fri Jun 7 14:11:31 2024 -0400

    Applied changes to prevent loss of data when unlatching on 917

examples/lock-app/silabs/include/LockManager.h
@@ -199,10 +199,12 @@ public:
 private:
     struct UnlatchContext
     {
+        static constexpr uint8_t kMaxPinLenght = UINT8_MAX;
+        uint8_t mPinBuffer[kMaxPinLenght];
+        size_t mPinLength;
         chip::EndpointId mEndpointId;
         Nullable<chip::FabricIndex> mFabricIdx;
         Nullable<chip::NodeId> mNodeId;
-        Optional<chip::ByteSpan> mPin;
         OperationErrorEnum mErr;
 
         void Update(chip::EndpointId endpointId, const Nullable<chip::FabricIndex> & fabricIdx,
@@ -211,8 +213,18 @@ private:
             mEndpointId = endpointId;
             mFabricIdx  = fabricIdx;
             mNodeId     = nodeId;
-            mPin        = pin;
             mErr        = err;
+
+            if (pin.HasValue())
+            {
+                memcpy(mPinBuffer, pin.Value().data(), pin.Value().size());
+                mPinLength = pin.Value().size();
+            }
+            else
+            {
+                memset(mPinBuffer, 0, kMaxPinLenght);
+                mPinLength = 0;
+            }
         }
     };
     UnlatchContext mUnlatchContext;
@@ -242,11 +254,7 @@ private:
     uint8_t mCredentialData[kNumCredentialTypes][kMaxCredentials][kMaxCredentialSize];
     CredentialStruct mCredentials[kMaxUsers][kMaxCredentials];
 
-    static LockManager sLock;
     EFR32DoorLock::LockInitParams::LockParam LockParams;
 };
 
-inline LockManager & LockMgr()
-{
-    return LockManager::sLock;
-}
+LockManager & LockMgr();

SiliconLabsSoftware/matter_sdk

examples/lock-app/silabs/include/LockManager.h
@@ -199,10 +199,12 @@ public:
 private:
     struct UnlatchContext
     {
+        static constexpr uint8_t kMaxPinLength = UINT8_MAX;
+        uint8_t mPinBuffer[kMaxPinLength];
+        uint8_t mPinLength;
         chip::EndpointId mEndpointId;
         Nullable<chip::FabricIndex> mFabricIdx;
         Nullable<chip::NodeId> mNodeId;
-        Optional<chip::ByteSpan> mPin;
         OperationErrorEnum mErr;
 
         void Update(chip::EndpointId endpointId, const Nullable<chip::FabricIndex> & fabricIdx,
@@ -211,8 +213,18 @@ private:
             mEndpointId = endpointId;
             mFabricIdx  = fabricIdx;
             mNodeId     = nodeId;
-            mPin        = pin;
             mErr        = err;
+
+            if (pin.HasValue())
+            {
+                memcpy(mPinBuffer, pin.Value().data(), pin.Value().size());
+                mPinLength = static_cast<uint8_t>(pin.Value().size());
+            }
+            else
+            {
+                memset(mPinBuffer, 0, kMaxPinLength);
+                mPinLength = 0;
+            }
         }
     };
     UnlatchContext mUnlatchContext;
@@ -242,11 +254,7 @@ private:
     uint8_t mCredentialData[kNumCredentialTypes][kMaxCredentials][kMaxCredentialSize];
     CredentialStruct mCredentials[kMaxUsers][kMaxCredentials];
 
-    static LockManager sLock;
     EFR32DoorLock::LockInitParams::LockParam LockParams;
 };
 
-inline LockManager & LockMgr()
-{
-    return LockManager::sLock;
-}
+LockManager & LockMgr();