84682d1165fef09746491eeac37fd92916d86b9b
lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
2024-11-04 13:32:13 -0500

    Use of Missing optionnal when pin length is 0

--- examples/lock-app/silabs/src/LockManager.cpp
+++ examples/lock-app/silabs/src/LockManager.cpp
@@ -264,8 +264,9 @@ void LockManager::UnlockAfterUnlatch()
     bool succes = false;
     if (mUnlatchContext.mEndpointId != kInvalidEndpointId)
     {
+        Optional<chip::ByteSpan> pin = (mUnlatchContext.mPinLength) ? MakeOptional(chip::ByteSpan(mUnlatchContext.mPinBuffer, mUnlatchContext.mPinLength)): Optional<chip::ByteSpan>::Missing();
         succes = setLockState(mUnlatchContext.mEndpointId, mUnlatchContext.mFabricIdx, mUnlatchContext.mNodeId,
-                              DlLockState::kUnlocked, MakeOptional(chip::ByteSpan(mUnlatchContext.mPinBuffer, mUnlatchContext.mPinLength)), mUnlatchContext.mErr);
+                              DlLockState::kUnlocked, pin, mUnlatchContext.mErr);
     }
 
     if (!succes)
