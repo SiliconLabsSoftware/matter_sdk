4c65efb5c81fa2c44713cc02d6f192e75aed57ef
bhmanda-silabs <107180296+bhmanda-silabs@users.noreply.github.com>
2024-12-17 00:33:08 +0530

    [SL-TEMP] Temp-fix for delay 917 NCP sleep till wifi-connection (#179)

--- examples/platform/silabs/wifi/icd/ApplicationSleepManager.cpp
+++ examples/platform/silabs/wifi/icd/ApplicationSleepManager.cpp
@@ -170,13 +170,19 @@ bool ApplicationSleepManager::ProcessKeychainEdgeCase()
 void ApplicationSleepManager::OnEnterActiveMode()
 {
     mIsInActiveMode = true;
+// TEMP-fix: Added SLI_SI91X_MCU_INTERFACE to delay 917 NCP sleep till wifi-connection
+#if (defined(SLI_SI91X_MCU_INTERFACE) && SLI_SI91X_MCU_INTERFACE == 1)
     mWifiSleepManager->VerifyAndTransitionToLowPowerMode();
+#endif // (defined(SLI_SI91X_MCU_INTERFACE) && SLI_SI91X_MCU_INTERFACE == 1)
 }
 
 void ApplicationSleepManager::OnEnterIdleMode()
 {
     mIsInActiveMode = false;
+// TEMP-fix: Added SLI_SI91X_MCU_INTERFACE to delay 917 NCP sleep till wifi-connection
+#if (defined(SLI_SI91X_MCU_INTERFACE) && SLI_SI91X_MCU_INTERFACE == 1)
     mWifiSleepManager->VerifyAndTransitionToLowPowerMode();
+#endif // (defined(SLI_SI91X_MCU_INTERFACE) && SLI_SI91X_MCU_INTERFACE == 1)
 }
 
 void ApplicationSleepManager::OnTransitionToIdle()
