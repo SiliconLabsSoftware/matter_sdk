2cbc5069ade435c740830a420b7991746e6bcc0c
Jean-Francois Penven <jean-francois.penven@silabs.com>
2025-04-04 10:30:25 -0400

    replace matter main with blinky to test build process

--- examples/platform/silabs/zephyr/src/main.cpp
+++ examples/platform/silabs/zephyr/src/main.cpp
@@ -15,23 +15,76 @@
  *    limitations under the License.
  */
 
-#include "AppTask.h"
+// #include "AppTask.h"
 
-#include <zephyr/logging/log.h>
+// #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(app, CONFIG_CHIP_APP_LOG_LEVEL);
+// LOG_MODULE_REGISTER(app, CONFIG_CHIP_APP_LOG_LEVEL);
 
-using namespace ::chip;
+// using namespace ::chip;
 
-int main()
-{
-    CHIP_ERROR err = CHIP_NO_ERROR;
+// int main()
+// {   
+//     // TODO MATTER-
 
-    if (err == CHIP_NO_ERROR)
-    {
-        err = chip::silabs::App::GetAppTask().Start();
-    }
+//     // CHIP_ERROR err = CHIP_NO_ERROR;
 
-    LOG_ERR("Exited with code %" CHIP_ERROR_FORMAT, err.Format());
-    return err == CHIP_NO_ERROR ? EXIT_SUCCESS : EXIT_FAILURE;
-}
+//     // if (err == CHIP_NO_ERROR)
+//     // {
+//     //     err = chip::silabs::App::GetAppTask().Start();
+//     // }
+
+//     // LOG_ERR("Exited with code %" CHIP_ERROR_FORMAT, err.Format());
+//     // return err == CHIP_NO_ERROR ? EXIT_SUCCESS : EXIT_FAILURE;
+// }
+
+
+// For the time being, simple run the standard blinky app from Zephyr
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+ #include <stdio.h>
+ #include <zephyr/kernel.h>
+ #include <zephyr/drivers/gpio.h>
+ 
+ /* 1000 msec = 1 sec */
+ #define SLEEP_TIME_MS   1000
+ 
+ /* The devicetree node identifier for the "led0" alias. */
+ #define LED0_NODE DT_ALIAS(led0)
+ 
+ /*
+  * A build error on this line means your board is unsupported.
+  * See the sample documentation for information on how to fix this.
+  */
+ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+ 
+ int main(void)
+ {
+     int ret;
+     bool led_state = true;
+ 
+     if (!gpio_is_ready_dt(&led)) {
+         return 0;
+     }
+ 
+     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+     if (ret < 0) {
+         return 0;
+     }
+ 
+     while (1) {
+         ret = gpio_pin_toggle_dt(&led);
+         if (ret < 0) {
+             return 0;
+         }
+ 
+         led_state = !led_state;
+         printf("LED state: %s\n", led_state ? "ON" : "OFF");
+         k_msleep(SLEEP_TIME_MS);
+     }
+     return 0;
+ }
