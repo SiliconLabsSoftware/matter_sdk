4d536bcda9cebb34f43215ca2a7a36b86dd07ab3
Arghya Dutta <ardutta@silabs.com>
2025-08-12 11:40:28 +0530

    Sample test with addition changes

--- test_sample.c
+++ test_sample.c
@@ -12,6 +12,16 @@ int add(int a, int b) {
 }
 
 /**
+ * @brief Calculate the difference of two integers
+ * @param a First integer
+ * @param b Second integer
+ * @return Difference of a and b
+ */
+int subtract(int a, int b) {
+    return a - b;
+}
+
+/**
  * @brief Calculate the factorial of a number
  * @param n The number to calculate factorial for
  * @return Factorial of n
@@ -29,6 +39,7 @@ long factorial(int n) {
 int main() {
     int x = 5, y = 3;
     printf("Sum of %d and %d is: %d\n", x, y, add(x, y));
+    printf("Difference of %d and %d is: %d\n", x, y, subtract(x, y));
     
     int fact_num = 5;
     printf("Factorial of %d is: %ld\n", fact_num, factorial(fact_num));
