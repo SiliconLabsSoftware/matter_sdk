6d645bad454fba62cff402dbe58e76c6e1c09dc5
sabollim-silabs <158290810+sabollim-silabs@users.noreply.github.com>
2025-08-15 23:32:43 +0530

    [SL-ONLY] Porting dic component to release 2.6 -1.4 (#562)
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Rohan Sahay <103027015+rosahay-silabs@users.noreply.github.com>

--- src/lwip/silabs/lwipopts-wf200.h
+++ src/lwip/silabs/lwipopts-wf200.h
@@ -70,19 +70,19 @@
 
 #define LWIP_SOCKET 0
 
-#define TCPIP_THREAD_PRIO osPriorityAboveNormal
-
-#ifdef DIC_ENABLE
+#ifdef SL_MATTER_ENABLE_AWS
 #define LWIP_DNS 1
 #define DNS_RAND_TXID() ((u32_t) rand())
 #define MEM_SIZE 5632
 #define MEMP_NUM_UDP_PCB (6)
 #define TCP_MSS (4 * 1152)
+#define TCPIP_THREAD_PRIO osPriorityHigh7
 #else
 #define LWIP_DNS 0
 #define MEMP_NUM_UDP_PCB (5)
 #define TCP_MSS (1152)
-#endif // DIC_ENABLE
+#define TCPIP_THREAD_PRIO osPriorityAboveNormal
+#endif // SL_MATTER_ENABLE_AWS
 
 #define LWIP_FREERTOS_USE_STATIC_TCPIP_TASK 1
 
