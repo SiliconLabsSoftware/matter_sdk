d0521340078fcde0f20305117e2b6907209b2d89
lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
2025-08-19 11:16:32 -0400

    Removed un-necessary configs

--- examples/base-platform-app/silabs/include/AppConfig.h
+++ examples/base-platform-app/silabs/include/AppConfig.h
@@ -26,7 +26,3 @@
 #define APP_TASK_NAME "Platform"
 
 #define BLE_DEV_NAME "SL-" APP_TASK_NAME
-
-// Time it takes in ms for the simulated actuator to move from one
-// state to another.
-#define ACTUATOR_MOVEMENT_PERIOS_MS 10
