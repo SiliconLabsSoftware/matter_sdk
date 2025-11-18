6284c971e918ad8731722b1b76e493d6433c1dc2
Matthew Albert <matthew.albert@silabs.com>
2025-11-18 10:18:56 -0500

    Update headers

--- src/platform/silabs/tracing/mocks/cmsis_os2_stub.h
+++ src/platform/silabs/tracing/mocks/cmsis_os2_stub.h
@@ -1,28 +1,19 @@
-/***************************************************************************/ /**
-                                                                               * @file cmsis_os2_stub.h
-                                                                               * @brief Stub header for CMSIS-OS2 API (for unit
-                                                                               *tests)
-                                                                               *******************************************************************************
-                                                                               * # License
-                                                                               * <b>Copyright 2025 Project CHIP Authors</b>
-                                                                               *******************************************************************************
-                                                                               *
-                                                                               * Licensed under the Apache License, Version 2.0 (the
-                                                                               *"License"); you may not use this file except in
-                                                                               *compliance with the License. You may obtain a copy
-                                                                               *of the License at
-                                                                               *
-                                                                               *     http://www.apache.org/licenses/LICENSE-2.0
-                                                                               *
-                                                                               * Unless required by applicable law or agreed to in
-                                                                               *writing, software distributed under the License is
-                                                                               *distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
-                                                                               *OR CONDITIONS OF ANY KIND, either express or
-                                                                               *implied. See the License for the specific language
-                                                                               *governing permissions and limitations under the
-                                                                               *License.
-                                                                               ******************************************************************************/
-
+/***************************************************************************
+ * @file cmsis_os2_stub.h
+ * @brief Stub header for CMSIS-OS2 API (for unit tests)
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2025 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
 #pragma once
 
 #include <stdint.h>
@@ -59,7 +50,6 @@ typedef enum
     osErrorISR       = -6
 } osStatus_t;
 
-// Function declarations (implementations provided by test mocks)
 osTimerId_t osTimerNew(osTimerFunc_t func, osTimerType_t type, void * argument, const osTimerAttr_t * attr);
 uint32_t osKernelGetTickFreq(void);
 osStatus_t osTimerStart(osTimerId_t timer_id, uint32_t ticks);
