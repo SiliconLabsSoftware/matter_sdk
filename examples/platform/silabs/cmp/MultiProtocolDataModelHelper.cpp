c4ebda198acb69bf6553881915f68ad6edd1df5e
Restyled.io <commits@restyled.io>
2025-12-19 16:26:00 +0000

    Restyled by clang-format

--- examples/platform/silabs/cmp/MultiProtocolDataModelHelper.cpp
+++ examples/platform/silabs/cmp/MultiProtocolDataModelHelper.cpp
@@ -76,7 +76,7 @@ void WriteMatterAttributeValueToZigbee(chip::EndpointId endpointId, chip::Cluste
     // TODO MATTER-5844  remove the check below once the Zigbee stack support overriding CLI and logs outputs.
     // They first need to decouple the output of both so that the Matter team can retrieve them separately.
     // Otherwise either the logs are blocking and causing major delays or the CLI output will be completely mangle
-    //and some string might be lost.
+    // and some string might be lost.
     if (sl_zigbee_af_contains_attribute(endpointId, mpClusterMetadata->zigbeeClusterId, mpAttributeMetadata->zigbeeAttributeId,
                                         CLUSTER_MASK_SERVER, mpAttributeMetadata->zigbeeMfgAttributeId))
     {
