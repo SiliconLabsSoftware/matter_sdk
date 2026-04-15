9a6cb61b118aa953c5c251db29dabb2fb26d90cb
Sarthak Shaha <sarthak.shaha@silabs.com>
2026-04-15 11:30:41 -0400

    remove redundant declaration

--- examples/lighting-app/silabs/src/DataModelCallbacks.cpp
+++ examples/lighting-app/silabs/src/DataModelCallbacks.cpp
@@ -137,23 +137,3 @@ void MatterPostAttributeChangeCallback(const chip::app::ConcreteAttributePath & 
     // CommonAppTask& so AppTaskImpl::DmCallbackMatterPostAttributeChangeCallback (CRTP) is used, not AppTask:: (static AppTask&).
     CommonAppTask::GetAppTask().DmCallbackMatterPostAttributeChangeCallback(attributePath, type, size, value);
 }
-
-/** @brief OnOff Cluster Init
- *
- * This function is called when a specific cluster is initialized. It gives the
- * application an opportunity to take care of cluster initialization procedures.
- * It is called exactly once for each endpoint where cluster is present.
- *
- * @param endpoint   Ver.: always
- *
- * TODO Issue #3841
- * emberAfOnOffClusterInitCallback happens before the stack initialize the cluster
- * attributes to the default value.
- * The logic here expects something similar to the deprecated Plugins callback
- * emberAfPluginOnOffClusterServerPostInitCallback.
- *
- */
-void emberAfOnOffClusterInitCallback(EndpointId endpoint)
-{
-    // TODO: implement any additional Cluster Server init actions
-}
