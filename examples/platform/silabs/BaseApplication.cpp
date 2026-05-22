38b9852c24f472c34fed36c7e622fa11eb137b98
sarthak shaha <sarthak.shaha@silabs.com>
2026-05-22 12:50:07 -0400

    [SL-ONLY] Add missing attribute write for zigbee (#971)
    * cmp fixes
    
    * cmp fixes
    
    * cmp fixes
    
    * cmp fixes
    
    * cmp fixes
    
    * cmp fixes
    
    * remove maybe unused
    
    * Update examples/platform/silabs/BaseApplication.cpp
    
    ---------
    
    Co-authored-by: Junior Martinez <67972863+jmartinez-silabs@users.noreply.github.com>

--- examples/platform/silabs/BaseApplication.cpp
+++ examples/platform/silabs/BaseApplication.cpp
@@ -117,6 +117,10 @@
 #include <TracingShellCommands.h>
 #endif // MATTER_TRACING_ENABLED
 
+#ifdef SL_CATALOG_ZIGBEE_ZCL_FRAMEWORK_CORE_PRESENT
+#include <MultiProtocolDataModelHelper.h>
+#endif // SL_CATALOG_ZIGBEE_ZCL_FRAMEWORK_CORE_PRESENT
+
 // sl-only
 #if defined(SL_MATTER_ENABLE_APP_SLEEP_MANAGER) && SL_MATTER_ENABLE_APP_SLEEP_MANAGER
 #include <ApplicationSleepManager.h>
@@ -1126,5 +1130,11 @@ void MatterPostAttributeChangeCallback(const chip::app::ConcreteAttributePath & 
 {
     // Route through CustomerAppTask / AppTaskImpl (CRTP) so overrides use DMPostAttributeChangeCallbackImpl.
     CustomerAppTask::GetAppTask().DMPostAttributeChangeCallback(attributePath, type, size, value);
+#ifdef SL_CATALOG_ZIGBEE_ZCL_FRAMEWORK_CORE_PRESENT
+    EndpointId endpointId   = attributePath.mEndpointId;
+    ClusterId clusterId     = attributePath.mClusterId;
+    AttributeId attributeId = attributePath.mAttributeId;
+    MultiProtocolDataModel::WriteMatterAttributeValueToZigbee(endpointId, clusterId, attributeId, value, type);
+#endif // SL_CATALOG_ZIGBEE_ZCL_FRAMEWORK_CORE_PRESENT
 }
 #endif // CHIP_SILABS_APP_USE_CUSTOMER_APP_TASK
