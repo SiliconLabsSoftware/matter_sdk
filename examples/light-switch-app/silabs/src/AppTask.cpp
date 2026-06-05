636ef1e43af75984e1dd7cb87bbadc885b962dfb
lpbeliveau-silabs <louis-philip.beliveau@silabs.com>
2026-06-05 11:47:11 -0400

    Made changes for clang ICD builds

--- examples/light-switch-app/silabs/src/AppTask.cpp
+++ examples/light-switch-app/silabs/src/AppTask.cpp
@@ -792,7 +792,7 @@ void AppTask::DMPostAttributeChangeCallback(const chip::app::ConcreteAttributePa
     [[maybe_unused]] AttributeId attributeId = attributePath.mAttributeId;
     ChipLogDetail(Zcl, "Cluster callback: " ChipLogFormatMEI, ChipLogValueMEI(clusterId));
 
-    if (clusterId == Identify::Id)
+    if (clusterId == Clusters::Identify::Id)
     {
         ChipLogProgress(Zcl, "Identify attribute ID: " ChipLogFormatMEI " Type: %u Value: %u, length %u",
                         ChipLogValueMEI(attributeId), type, *value, size);
