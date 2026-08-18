994a9fdc38c75f04bf3fd9739bcd63933c57289b
Jean-Francois Penven <jean-francois.penven@silabs.com>
2026-08-18 09:57:53 -0400

    Add Matter CPC implementation

---
+++ examples/platform/silabs/matter_cpc.cpp
@@ -0,0 +1,194 @@
+/***************************************************************************//**
+ * @brief RTOS interface for Matter CPC driver
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2026 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ ******************************************************************************/
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cmsis_os2.h>
+#include "sl_cmsis_os2_common.h"
+#include "sl_component_catalog.h"
+#include "sl_core.h"
+#include "sl_status.h"
+#include "sli_cpc.h"
+#include "matter_cpc.h"
+
+#include "Assertions.h"
+
+#define SL_MATTER_CPC_RTOS_TASK_PRIORITY osPriorityRealtime3
+#define SL_MATTER_CPC_RTOS_TASK_STACK_SIZE 1000
+
+
+#ifndef SL_CPC_ENDPOINT_MATTER_NCP_ID
+#define SL_CPC_ENDPOINT_MATTER_NCP_ID SL_CPC_ENDPOINT_USER_ID_0 // 90
+#endif
+
+void sl_matter_cpc_handle_msg(void);
+
+static osSemaphoreId_t matter_cpc_signal_semaphore;
+
+void sl_matter_cpc_on_transport_notify(uint8_t endpoint_id, void * arg)
+{
+  (void)endpoint_id;
+  (void)arg;
+
+  osSemaphoreRelease(matter_cpc_signal_semaphore);
+}
+
+
+static sl_matter_cpc_state_t cpc_state = SL_MATTER_CPC_STATE_DISCONNECTED;
+static sl_cpc_endpoint_handle_t endpoint_handle;
+
+int sl_matter_cpc_wait_for_activity()
+{
+  osSemaphoreAcquire(matter_cpc_signal_semaphore, osWaitForever);
+  return 1;//CPC-RTOS calls read packet only if semaphore is set
+}
+
+void sl_matter_cpc_tx_callback(sl_cpc_user_endpoint_id_t endpoint_id, void *buffer, void *arg, sl_status_t status)
+{
+  (void)endpoint_id;
+  (void)buffer;
+  (void)arg;
+//   TODO MATTER specific behavior.
+//   hci_common_transport_transmit_complete(status);
+}
+
+sl_status_t sl_matter_cpc_init(void)
+{
+  sl_status_t status = SL_STATUS_OK;
+  // TODO remove me when we have a service endpoint
+  status = sl_cpc_init_user_endpoint(&endpoint_handle, SL_CPC_ENDPOINT_MATTER_NCP_ID, 0);
+
+  // status = sli_cpc_init_service_endpoint(&endpoint_handle, SL_CPC_ENDPOINT_MATTER_NCP_ID, 0);
+  VerifyOrReturnError(status == SL_STATUS_OK, status);
+
+  status = sl_cpc_set_endpoint_option(&endpoint_handle, SL_CPC_ENDPOINT_ON_IFRAME_WRITE_COMPLETED, (void *)sl_matter_cpc_tx_callback);
+  VerifyOrReturnError(status == SL_STATUS_OK, status);
+  status = sl_cpc_set_endpoint_option(&endpoint_handle, SL_CPC_ENDPOINT_ON_IFRAME_RECEIVE, (void *)sl_matter_cpc_on_transport_notify);
+  VerifyOrReturnError(status == SL_STATUS_OK, status);
+  status = sl_cpc_set_endpoint_option(&endpoint_handle, SL_CPC_ENDPOINT_ON_CONNECT, (void*)sl_matter_cpc_on_connect);
+  VerifyOrReturnError(status == SL_STATUS_OK, status);
+  status = sl_cpc_set_endpoint_option(&endpoint_handle, SL_CPC_ENDPOINT_ON_ERROR, (void*)sl_matter_cpc_error);
+  VerifyOrReturnError(status == SL_STATUS_OK, status);
+
+  if (matter_cpc_signal_semaphore == NULL) {
+    matter_cpc_signal_semaphore = osSemaphoreNew(1, 0, NULL);
+    status = (matter_cpc_signal_semaphore != NULL) ? SL_STATUS_OK : SL_STATUS_FAIL;
+  }
+
+  return status;
+}
+
+void sl_matter_cpc_on_connect(uint8_t endpoint_id, void *arg)
+{
+  (void)endpoint_id;
+  (void)arg;
+
+  // Endpoint is connected. Allow message transmission.
+  // TODO MATTER
+//   hci_common_transport_transmit_reconnected();
+  cpc_state = SL_MATTER_CPC_STATE_CONNECTED;
+}
+
+void sl_matter_cpc_error(uint8_t endpoint_id, void *arg)
+{
+  (void)endpoint_id;
+  (void)arg;
+  sl_status_t status;
+
+  cpc_state = SL_MATTER_CPC_STATE_DISCONNECTED;
+  status = sl_cpc_terminate_endpoint(&endpoint_handle, 0);
+  EFM_ASSERT(status == SL_STATUS_OK);
+  sl_matter_cpc_on_transport_notify(0, NULL);
+}
+
+int sl_matter_cpc_read(uint8_t **read_buf)
+{
+  if (read_buf == NULL) {
+    return -1;
+  }
+
+  void *   data        = NULL;
+  uint16_t data_length = 0;
+
+  // Non-blocking: callers are expected to gate this with sl_matter_cpc_wait_for_new_data().
+  sl_status_t status = sl_cpc_read(&endpoint_handle, &data, &data_length, 0, SL_CPC_FLAG_NO_BLOCK);
+
+  if (status == SL_STATUS_EMPTY) {
+    *read_buf = NULL;
+    return 0;
+  }
+  if (status != SL_STATUS_OK) {
+    *read_buf = NULL;
+    return -1;
+  }
+
+  *read_buf = (uint8_t *) data;
+  return (int) data_length;
+}
+
+sl_status_t sl_matter_cpc_write(uint8_t *data, uint16_t len)
+{
+  return sl_cpc_write(&endpoint_handle, data, len, 0, NULL);
+}
+
+void sl_matter_cpc_free(void *buf)
+{
+  sl_cpc_free_rx_buffer((void *) buf);
+}
+
+bool sl_matter_is_cpc_waiting(void)
+{
+  // Any state other than SL_CPC_STATE_CONNECTED is a disconnected state.
+  return (cpc_state == SL_MATTER_CPC_STATE_CONNECTING);
+}
+
+bool sl_matter_is_cpc_connected(void)
+{
+  // Any state other than SL_CPC_STATE_CONNECTED is a disconnected state.
+  return (cpc_state == SL_MATTER_CPC_STATE_CONNECTED);
+}
+
+void sl_matter_reconnect_cpc(void)
+{
+  switch (cpc_state) {
+    case SL_MATTER_CPC_STATE_DISCONNECTED:
+      sl_cpc_listen_endpoint(&endpoint_handle, SL_CPC_FLAG_NO_BLOCK);
+      cpc_state = SL_MATTER_CPC_STATE_CONNECTING;
+      break;
+
+    case SL_MATTER_CPC_STATE_CONNECTING:
+    case SL_MATTER_CPC_STATE_CONNECTED:
+      // Nothing to do
+      break;
+
+    default:
+      EFM_ASSERT(false);
+  }
+}
