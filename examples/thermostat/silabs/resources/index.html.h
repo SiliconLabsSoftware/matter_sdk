03709aac1b256dd1aa3a6c3687981682377b856b
rosahay-silabs <103027015+rosahay-silabs@users.noreply.github.com>
2026-08-18 11:06:34 +0530

    chore(silabs): update index.html.h file header with new description and copyright year

--- examples/thermostat/silabs/resources/index.html.h
+++ examples/thermostat/silabs/resources/index.html.h
@@ -1,19 +1,20 @@
 /*******************************************************************************
-* @file  index.html.h
-* @brief 
-*******************************************************************************
-* # License
-* <b>Copyright 2023 Silicon Laboratories Inc. www.silabs.com</b>
-*******************************************************************************
-*
-* The licensor of this software is Silicon Laboratories Inc. Your use of this
-* software is governed by the terms of Silicon Labs Master Software License
-* Agreement (MSLA) available at
-* www.silabs.com/about-us/legal/master-software-license-agreement. This
-* software is distributed to you in Source Code format and is governed by the
-* sections of the MSLA applicable to Source Code.
-*
-******************************************************************************/
+ * @file  index.html.h
+ * @brief HTTP page for the HTTP offload client application,
+ * this page is uploaded to the device and displayed in the web browser.
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2026 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of Silicon Labs Master Software License
+ * Agreement (MSLA) available at
+ * www.silabs.com/about-us/legal/master-software-license-agreement. This
+ * software is distributed to you in Source Code format and is governed by the
+ * sections of the MSLA applicable to Source Code.
+ *
+ ******************************************************************************/
 
 const unsigned char sl_index[] =
   "<html><head><style "
