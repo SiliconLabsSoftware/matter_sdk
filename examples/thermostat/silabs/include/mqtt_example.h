e12d56557c6cc766340fd5a1702c7e725c05ea1e
rosahay-silabs <103027015+rosahay-silabs@users.noreply.github.com>
2026-08-20 20:59:14 +0530

    Update mqtt_example.h to include Apache License 2.0 and enhance documentation with a detailed MQTT client usage guide.

--- examples/thermostat/silabs/include/mqtt_example.h
+++ examples/thermostat/silabs/include/mqtt_example.h
@@ -1,10 +1,29 @@
-/***************************************************************************/
-/**
- * @file mqtt_example.h
+/*
+ *
+ *    Copyright (c) 2026 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
  * @brief Host LwIP Paho MQTT/MQTTS demo entry point
- *******************************************************************************
- * SPDX-License-Identifier: Zlib
- ******************************************************************************/
+ * 1. Init the MqttClient
+ * 2. Connect to the MQTT broker
+ * 3. Subscribe to the topic
+ * 4. Publish the message
+ * 5. Disconnect from the MQTT broker (optional)
+ * 6. Deinit the MqttClient (optional)
+ */
+
 #pragma once
 
 #include "sl_status.h"
