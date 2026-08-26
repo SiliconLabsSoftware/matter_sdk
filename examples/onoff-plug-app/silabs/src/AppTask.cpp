23a4fd465145ba3f01d73586eaf2a06d6c396818
chirag-silabs <chirag.bansal@silabs.com>
2026-08-26 11:08:03 +0530

    [SL-ONLY] Update the QR code define for sl only apps

--- examples/onoff-plug-app/silabs/src/AppTask.cpp
+++ examples/onoff-plug-app/silabs/src/AppTask.cpp
@@ -114,7 +114,7 @@ CHIP_ERROR AppTask::AppInit()
 // Update the LCD with the Stored value. Show QR Code if not provisioned
 #ifdef DISPLAY_ENABLED
     GetLCD().WriteDemoUI(sPlugOn);
-#ifdef QR_CODE_ENABLED
+#if SL_MATTER_QR_CODE_ENABLED
 #ifdef SL_WIFI
     if (!ConnectivityMgr().IsWiFiStationProvisioned())
 #else
@@ -123,7 +123,7 @@ CHIP_ERROR AppTask::AppInit()
     {
         GetLCD().ShowQRCode(true);
     }
-#endif // QR_CODE_ENABLED
+#endif // SL_MATTER_QR_CODE_ENABLED
 #endif
 
     return err;
