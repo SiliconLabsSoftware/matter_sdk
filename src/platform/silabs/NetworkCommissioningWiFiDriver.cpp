c5385f88f64efd986c9ba04b6bc43b745873d5c5
rosahay-silabs <103027015+rosahay-silabs@users.noreply.github.com>
2026-09-15 22:08:39 +0530

    refactor(silabs-wifi): enable WiFi station mode during network connection setup

--- src/platform/silabs/NetworkCommissioningWiFiDriver.cpp
+++ src/platform/silabs/NetworkCommissioningWiFiDriver.cpp
@@ -169,9 +169,6 @@ CHIP_ERROR SlWiFiDriver::ConnectWiFiNetwork(const char * ssid, uint8_t ssidLen, 
     memcpy(wifiConfig.key, key, keyLen);
     wifiConfig.keyLen = keyLen;
 
-    // dummy value which is overridden by the actual security value when connecting to the access point
-    wifiConfig.security.Set(chip::app::Clusters::NetworkCommissioning::WiFiSecurityBitmap::kWpa2Personal);
-
     ChipLogProgress(NetworkProvisioning, "Setting up connection for WiFi SSID: %s", NullTerminated(ssid, ssidLen).c_str());
 
     ReturnErrorOnFailure(WifiInterface::GetInstance().SetWifiCredentials(wifiConfig));
