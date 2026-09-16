a5e473e2714ad4a773360b97288b790a35f7192a
Rohan Sahay <103027015+rosahay-silabs@users.noreply.github.com>
2026-09-16 11:17:19 +0530

    Apply suggestion from @jmartinez-silabs
    Co-authored-by: Junior Martinez <67972863+jmartinez-silabs@users.noreply.github.com>

--- src/platform/silabs/wifi/SiWx/WifiInterfaceImpl.cpp
+++ src/platform/silabs/wifi/SiWx/WifiInterfaceImpl.cpp
@@ -36,7 +36,7 @@
 #include <inet/UDPEndPointImplLwIP.h>
 // TODO: Remove nogncheck once we have a proper way to include this file.
 #include <platform/CHIPDeviceLayer.h> // nogncheck
-#endif                                // SL_INET_CONFIG_UDP_LWIP_QUEUE_UNTIL_NETIF_READY
+#endif // SL_INET_CONFIG_UDP_LWIP_QUEUE_UNTIL_NETIF_READY
 #include <lib/support/CHIPMem.h>
 #include <lib/support/CHIPMemString.h>
 #include <lib/support/logging/CHIPLogging.h>
