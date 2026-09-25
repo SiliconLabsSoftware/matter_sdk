c2d2c04b4fd22163bbe739d43fd181c35edf81f7
rosahay-silabs <103027015+rosahay-silabs@users.noreply.github.com>
2026-09-25 19:36:34 +0530

    Update feature bitmaps for improved clarity and functionality.

--- src/platform/silabs/wifi/SiWx/SlNetConfig.cpp
+++ src/platform/silabs/wifi/SiWx/SlNetConfig.cpp
@@ -66,8 +66,8 @@ void SLApplyWiFiDeviceConfiguration(sl_wifi_device_configuration_t * configurati
 #endif // !(SL_MATTER_ENABLE_DUAL_STACK)
 
 #if defined(SL_MATTER_ENABLE_DUAL_STACK) && SL_MATTER_ENABLE_DUAL_STACK
-    // Enable basic SSL support for dual stack
-    configuration->boot_config.tcp_ip_feature_bit_map |= SL_SI91X_TCP_IP_FEAT_SSL;
+    // Enable dual mode support for dual stack
+    configuration->boot_config.tcp_ip_feature_bit_map |= SL_SI91X_EXT_TCP_IP_DUAL_MODE_ENABLE;
 
     // Enable DHCPv4 client, DNS client, and ICMP support for dual stack (required for IPv4)
     configuration->boot_config.tcp_ip_feature_bit_map |=
@@ -76,9 +76,8 @@ void SLApplyWiFiDeviceConfiguration(sl_wifi_device_configuration_t * configurati
     // Enable IPv6 and DHCPv6 client support for dual stack
     configuration->boot_config.tcp_ip_feature_bit_map |= (SL_SI91X_TCP_IP_FEAT_IPV6 | SL_SI91X_TCP_IP_FEAT_DHCPV6_CLIENT);
 
-    // Enable dual mode support for dual stack and SSL memory cloud support with more RAM for certificate storage
-    configuration->boot_config.ext_tcp_ip_feature_bit_map |=
-        (SL_SI91X_EXT_TCP_IP_DUAL_MODE_ENABLE | SL_SI91X_EXT_TCP_IP_FEAT_SSL_MEMORY_CLOUD);
+    // Enable basic SSL and SSL memory cloud support with more RAM for certificate storage
+    configuration->boot_config.ext_tcp_ip_feature_bit_map |= (SL_SI91X_TCP_IP_FEAT_SSL | SL_SI91X_EXT_TCP_IP_FEAT_SSL_MEMORY_CLOUD);
 #endif // SL_MATTER_ENABLE_DUAL_STACK
 
 #if defined(SL_MATTER_NEUTRAL_LESS_SWITCH_WIFI) && SL_MATTER_NEUTRAL_LESS_SWITCH_WIFI
